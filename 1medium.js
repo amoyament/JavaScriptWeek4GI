c9bbe0e6c590931cbd109925b66ac8e769b40147
Aubrey Moya-Mendez Trotter <Aubrey@adminisatorsmbp.mynetworksettings.com>
2023-05-16 17:25:52 -0400

    Completed hard challenge

--- 1medium.js
+++ 1medium.js
@@ -28,4 +28,3 @@ function findTarget(numsArray, targetNum) {
 
 console.log(findTarget(numsArray, targetNum)); // Output 4
 console.log(findTarget([4, 5, 6, 7, 0, 1, 2], 3)); // Output -1
-console.log(findTarget([7, 8, 9, 0, 1, 2, 3, 4, 5, 6], 5));
